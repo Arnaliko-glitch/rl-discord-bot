ae0c102c15704c564ef1696c6214aedb5a5e20ac
Arnaliko-glitch <66493778+Arnaliko-glitch@users.noreply.github.com>
2026-05-11 11:16:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,9 +45,11 @@ async def rank(interaction: discord.Interaction, player: str):
 
     r = requests.get(url, headers=headers)
 
-    print(r.status_code)
-print(r.text)
+    r = requests.get(url, headers=headers)
 
+    print(r.status_code)
+    print(r.text)
+    
     if r.status_code != 200:
         await interaction.response.send_message("Joueur introuvable.")
         return
